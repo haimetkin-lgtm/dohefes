31a13ace064829fb56c2c6d1227db0220d47f56b
Hai Metkin <haimetkin@gmail.com>
2026-08-24 00:23:00 +0300

    הוספת Google Analytics (gtag.js)
    אותו דפוס בדיוק כמו rami/insure-vda, Next.js Script עם strategy
    afterInteractive, מזהה מדידה ייעודי ל-dohefes.

--- app/layout.tsx
+++ app/layout.tsx
@@ -100,6 +100,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           crossOrigin="anonymous"
           strategy="afterInteractive"
         />
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-GKG9BFQ827" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-GKG9BFQ827');`}
+        </Script>
       </body>
     </html>
   );
